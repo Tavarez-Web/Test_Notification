820c1e64d71d019a9ed31031393a8bddb748f963
Elder Tavarez <eldertavarez@Elders-MacBook-Pro.local>
2023-06-08 15:17:56 -0400

    Se muestras notificaciones en primer plano

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:convert';
-
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
@@ -16,7 +14,7 @@ void main() async {
   await DatabaseHelper.instance.initializeDatabase();
 
   try {
-    FirebaseMessaging.onBackgroundMessage(firebaseMessagingBackgroundHandler);
+  FirebaseMessaging.onBackgroundMessage(firebaseMessagingBackgroundHandler);
   } catch (e) {
     // print(e);
   }
@@ -37,7 +35,7 @@ void main() async {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key});
 
   @override
   Widget build(BuildContext context) {
--- lib/noti.dart
+++ lib/noti.dart
@@ -220,7 +220,7 @@
 // }
 
 
-
+import 'dart:convert';
 import 'dart:developer';
 import 'dart:io';
 import 'package:firebase_messaging/firebase_messaging.dart';
@@ -228,56 +228,54 @@ import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:http/http.dart' as http;
 import 'package:path_provider/path_provider.dart';
 
-// import '../model/notification_model.dart';
-// import 'config/router/app_router.dart';
 import 'config/router/app_router.dart';
 import 'db.helper.dart';
 import 'notification_model.dart';
-// import 'notification_model.dart';
 
 Future<void> firebaseMessagingBackgroundHandler(RemoteMessage message) async {
   FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
       FlutterLocalNotificationsPlugin();
-  Map<String, dynamic> snsMessage = message.data;
-  var _snsMessage = NotificationModel.fromJsonNotification(snsMessage);
+    var data = message.data;
+    var _data = NotificationModel.fromJsonNotification(data);
   if (flutterLocalNotificationsPlugin == null) {
     flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
   }
-  if (_snsMessage.imageUrl != null &&
-      _snsMessage.imageUrl != null) {
+  if (_data.imageUrl != null &&
+      _data.imageUrl != null) {
     log("h2");
+    print("la _data${_data.message}");
     return NotificationHandler().showBigPictureNotification(message);
-  } else if (_snsMessage.imageUrl != null) {
+  } else if (_data.imageUrl != null) {
     return NotificationHandler()
-        .showBigPictureNotificationHiddenLargeIcon(snsMessage);
+        .showBigPictureNotificationHiddenLargeIcon(message);
   } else
-    flutterLocalNotificationsPlugin.show(
-        _snsMessage.message.hashCode,
-        _snsMessage.title,
-        _snsMessage.message,
+   await flutterLocalNotificationsPlugin.show(
+        _data.message.hashCode,
+        _data.title,
+        _data.message,
         NotificationDetails(
           android: AndroidNotificationDetails(
             'high_importance_channel', // id
-            'High Importance Notifications', // title
+            'Notificaciones de alta importancia', // título
             channelDescription:
-                'This channel is used for important notifications.',
-            // description
+                'Este canal se utiliza para notificaciones importantes.',
+            // descripción
             importance: Importance.high,
             icon: null, // android?.smallIcon,
-            // other properties...
+            // otras propiedades...
           ),
           iOS: DarwinNotificationDetails(
             presentAlert: true,
             presentBadge: true,
             presentSound: true,
-            subtitle: _snsMessage.title,
+            subtitle: _data.title,
             sound: "default",
             threadIdentifier:
-                _snsMessage.message.hashCode.toString(),
+                _data.message.hashCode.toString(),
           ),
         ));
   await DatabaseHelper.instance.initializeDatabase();
-  DatabaseHelper.instance.insertNotification(_snsMessage);
+  DatabaseHelper.instance.insertNotification(_data);
 }
 
 class NotificationHandler {
@@ -290,159 +288,118 @@ class NotificationHandler {
     // if (!kIsWeb) {
     channel = const AndroidNotificationChannel(
       'high_importance_channel', // id
-      'High Importance Notifications', // title
-      description: 'This channel is used for important notifications.',
-      // description
+      'Notificaciones de alta importancia', // título
+      description: 'Este canal se utiliza para notificaciones importantes.',
+      // descripción
       importance: Importance.high,
     );
-    var initializationSettingsAndroid =
-        AndroidInitializationSettings('@mipmap/ic_launcher');
 
-    var initializationSettings = InitializationSettings(
-      android: initializationSettingsAndroid,
-      iOS: DarwinInitializationSettings(
-          requestAlertPermission: true,
-          requestBadgePermission: true,
-          requestSoundPermission: true,
-         onDidReceiveLocalNotification:
-              (int id, String? title, String? body, String? payload) async {
-            log("onDidReceiveLocalNotification $id $title $body $payload");
-          }),
-    );
-    flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
-    flutterLocalNotificationsPlugin.initialize(initializationSettings,
-        onDidReceiveNotificationResponse:
-            NotificationHandler.onDidReceiveNotificationResponse);
-
-    /// Create an Android Notification Channel.
-    ///
-    /// We use this channel in the `AndroidManifest.xml` file to override the
-    /// default FCM channel to enable heads up notifications.
     flutterLocalNotificationsPlugin
         .resolvePlatformSpecificImplementation<
             AndroidFlutterLocalNotificationsPlugin>()
         ?.createNotificationChannel(channel);
+    // }
+  }
 
-    flutterLocalNotificationsPlugin.resolvePlatformSpecificImplementation<
-        IOSFlutterLocalNotificationsPlugin>();
-
-    FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
-      alert: true,
-      badge: true,
-      sound: true,
+  Future<dynamic> firebaseMessagingForegroundHandler(
+      RemoteMessage message) async {
+    FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+        FlutterLocalNotificationsPlugin();
+    var data = message.data;
+    var _data = NotificationModel.fromJsonNotification(data);
+    var androidPlatformChannelSpecifics = AndroidNotificationDetails(
+      'high_importance_channel', // id
+      'Notificaciones de alta importancia', // título
+      // description: 'Este canal se utiliza para notificaciones importantes.',
+      // descripción
+      importance: Importance.high,
+      icon: null, // android?.smallIcon,
+      // otras propiedades...
     );
-    getToken();
+    var iOSPlatformChannelSpecifics = DarwinNotificationDetails(
+      presentAlert: true,
+      presentBadge: true,
+      presentSound: true,
+      subtitle: _data.title,
+      sound: "default",
+      threadIdentifier: _data.message.hashCode.toString(),
+    );
+    var platformChannelSpecifics = NotificationDetails(
+      android: androidPlatformChannelSpecifics,
+      iOS: iOSPlatformChannelSpecifics,
+    );
+
+    await flutterLocalNotificationsPlugin.show(
+        _data.message.hashCode,
+        _data.title,
+        _data.message,
+        platformChannelSpecifics,
+        payload: jsonEncode(message.data));
   }
 
-  Future<void> getToken() async {
-    var token = await FirebaseMessaging.instance.getToken();
-    log("token =$token");
-  }
+ Future<void> showBigPictureNotification(RemoteMessage message) async {
+  var image = message.data?["image"] as String?;
+  var title = message.data?["title"] as String?;
 
-  Future<void> firebaseMessagingForegroundHandler(RemoteMessage message) {
-    Map<String, dynamic> snsMessage = message.data;
-    var _snsMessage = NotificationModel.fromJsonNotification(snsMessage);
-
-    if (_snsMessage.imageUrl != null &&
-        _snsMessage.imageUrl != null) {
-      return showBigPictureNotification(message);
-    } else if (_snsMessage.imageUrl != null) {
-      return showBigPictureNotificationHiddenLargeIcon(snsMessage);
-    } else {
-      return flutterLocalNotificationsPlugin.show(
-        _snsMessage.message.hashCode,
-        _snsMessage.title,
-        _snsMessage.message,
-        const NotificationDetails(
-          android: AndroidNotificationDetails(
-            'high_importance_channel', // id
-            'High Importance Notifications', // title
-            channelDescription:
-                'This channel is used for important notifications.',
-            // descriptio
-            importance: Importance.high,
-            icon: null,
-          ),
-          iOS: DarwinNotificationDetails(
-            presentAlert: true,
-            presentBadge: true,
-            presentSound: true,
-          ),
-        ),
-      );
-    }
-  }
-
-  Future<void> showBigPictureNotification(RemoteMessage message) async {
-     Map<String, dynamic> snsMessage = message.data;
-    var _snsMessage = NotificationModel.fromJsonNotification(snsMessage);
-
-    //  Map<String, dynamic> snsMessage = message.data;
-    final String largeIconPath = await _downloadAndSaveFile(
-        _snsMessage.imageUrl, 'largeIcon');
-    final String bigPicturePath = await _downloadAndSaveFile(
-        _snsMessage.imageUrl, 'bigPicture');
-    final BigPictureStyleInformation bigPictureStyleInformation =
-        BigPictureStyleInformation(FilePathAndroidBitmap(bigPicturePath),
-            largeIcon: FilePathAndroidBitmap(largeIconPath),
-            contentTitle:
-                '<b>${_snsMessage.title}</b>',
-            htmlFormatContentTitle: true,
-            summaryText: '${_snsMessage.message}',
-            htmlFormatSummaryText: true);
-    final AndroidNotificationDetails androidPlatformChannelSpecifics =
-        AndroidNotificationDetails(
-            'big text channel id', 'big text channel name',
-            channelDescription: 'big text channel description',
-            styleInformation: bigPictureStyleInformation);
-    final NotificationDetails platformChannelSpecifics =
+  if (image != null && title != null) {
+    var filePath = await _downloadAndSaveFile(image, title);
+    var bigPictureStyleInformation = BigPictureStyleInformation(
+      FilePathAndroidBitmap(filePath),
+    );
+    var androidPlatformChannelSpecifics = AndroidNotificationDetails(
+        'big_picture_channel', 'Notificaciones con imágenes grandes',
+        channelDescription:
+            'Este canal se utiliza para notificaciones con imágenes grandes.',
+        styleInformation: bigPictureStyleInformation);
+    var platformChannelSpecifics =
         NotificationDetails(android: androidPlatformChannelSpecifics);
     await flutterLocalNotificationsPlugin.show(
-        _snsMessage.id.hashCode,
-        _snsMessage.title,
-        _snsMessage.message,
-        platformChannelSpecifics);
+      title.hashCode,
+      title,
+      message.data!["message"] as String,
+      platformChannelSpecifics,
+      payload: jsonEncode(message.data),
+    );
   }
+}
 
-  Future<void> showBigPictureNotificationHiddenLargeIcon(
-      Map<String, dynamic> pinpointMessage) async {
-    final String bigPicturePath = await _downloadAndSaveFile(
-        pinpointMessage["pinpoint.notification.imageUrl"], 'bigPicture');
-    final BigPictureStyleInformation bigPictureStyleInformation =
-        BigPictureStyleInformation(FilePathAndroidBitmap(bigPicturePath),
-            hideExpandedLargeIcon: true,
-            contentTitle:
-                '<b>${pinpointMessage["pinpoint.notification.title"]}</b>',
-            htmlFormatContentTitle: true,
-            summaryText: '${pinpointMessage["pinpoint.notification.body"]}',
-            htmlFormatSummaryText: true);
-    final AndroidNotificationDetails androidPlatformChannelSpecifics =
-        AndroidNotificationDetails(
-            'big text channel id', 'big text channel name',
-            channelDescription: 'big text channel description',
-            styleInformation: bigPictureStyleInformation);
-    final NotificationDetails platformChannelSpecifics =
-        NotificationDetails(android: androidPlatformChannelSpecifics);
-    await flutterLocalNotificationsPlugin.show(
-        pinpointMessage["pinpoint.campaign.campaign_id"].hashCode,
-        pinpointMessage["pinpoint.notification.title"],
-        pinpointMessage["pinpoint.notification.body"],
-        platformChannelSpecifics);
-  }
 
-  Future<String> _downloadAndSaveFile(String url, String fileName) async {
-    final Directory directory = await getApplicationDocumentsDirectory();
-    final String filePath = '${directory.path}/$fileName';
-    if ( url.isEmpty) return '';
-    final http.Response response = await http.get(Uri.parse(url));
-    final File file = File(filePath);
+  Future<String> _downloadAndSaveFile(
+      String url, String fileName) async {
+    final directory = await getApplicationDocumentsDirectory();
+    final filePath = '${directory.path}/$fileName';
+    final response = await http.get(Uri.parse(url));
+    final file = File(filePath);
     await file.writeAsBytes(response.bodyBytes);
     return filePath;
   }
 
-  static void onDidReceiveNotificationResponse(NotificationResponse response) {
+  Future<void> showBigPictureNotificationHiddenLargeIcon(
+      RemoteMessage message) async {
+    var bigPictureStyleInformation = BigPictureStyleInformation(
+      FilePathAndroidBitmap(await _downloadAndSaveFile(
+          message.data!["image"] as String,
+          message.data!["title"] as String)),
+      hideExpandedLargeIcon: true,
+    );
+    var androidPlatformChannelSpecifics = AndroidNotificationDetails(
+        'big_picture_channel_hidden_large_icon',
+        'Notificaciones con imágenes grandes (icono grande oculto)',
+        channelDescription:
+            'Este canal se utiliza para notificaciones con imágenes grandes donde el icono grande está oculto.',
+        styleInformation: bigPictureStyleInformation);
+    var platformChannelSpecifics =
+        NotificationDetails(android: androidPlatformChannelSpecifics);
+    await flutterLocalNotificationsPlugin.show(
+      message.data!["title"].hashCode,
+      message.data!["title"] as String,
+      message.data!["message"] as String,
+      platformChannelSpecifics,
+      payload: jsonEncode(message.data),
+    );
+  }
+   static void onDidReceiveNotificationResponse(NotificationResponse response) {
     print(response.id);
-  
     appRouter.push('/push-details/${response.id.toString()}');
   }
 }
